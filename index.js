d548f6b7bea85f635f6bd0f7a40d44d28e919835
VnkHO <ho.vincent.nk@gmail.com>
2020-02-14 15:52:42 +0100

    index js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Made by Vnk HO')
+console.log(`Made by Vnk HO \n
+    Looking for an internship in March for 5-6months.\n
+    Position: Developer JS (front, back, fullstack) or in UX Design.\n
+    Link: github: https://github.com/VnkHO, behance: https://www.behance.net/hovincent, dribbble: https://dribbble.com/VnkHO \n
+    Personal website: in construction
+`)
